ba5f498b5dfc05b8cc9bccec18976a0d1aaef3fc
etancourt Maldonad <j.etancourt.maldonad@accenture.com>
2022-01-20 18:46:42 -0500

    Variables project

---
+++ Variables/PrimerApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string var;
+            var = "Hola";
+            Console.WriteLine(var);
+
+            bool var2;
+            var2 = false;
+            Console.WriteLine(var2);
+
+            char var3;
+            var3 = 'a';
+            Console.WriteLine(var3);
+
+            int var4;
+            var4 = 3;
+            Console.WriteLine(var4);
+
+            long var5;
+            var5 = 2147483638L;
+            Console.WriteLine(var5);
+
+            float var6;
+            var6 = 3.1413F;
+            Console.WriteLine(var6);
+
+            double var7;
+            var7 = 3.1413898491849118395;
+            Console.WriteLine(var7);
+
+            decimal var8;
+            var8 = 3.1413898491849118395m;
+            Console.WriteLine(var8);
+
+            Console.ReadLine();
+        }
+    }
+}

735d2829027c76dee3e80268e243fc82750eb36e
jpbmdev <jpbmdev@gmail.com>
2022-01-20 22:18:14 -0500

    methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hola");
+            LimpiarPantalla();
+
+            Console.WriteLine("Hola 2");
+            LimpiarPantalla();
+
+            Console.WriteLine("Hola 3");
+            LimpiarPantalla();
+            */
+
+            /*
+            Console.Write("Cual es tu nombre: ");
+            string minombre = Console.ReadLine();
+            Saludar(minombre);
+            */
+
+            int resultado = Sumar(10, 8);
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+
+        static void LimpiarPantalla()
+        {
+            Console.ReadLine();
+            Console.Clear();
+            Console.Beep();
+        }
+
+        static void Saludar(string nombre)
+        {
+            Console.WriteLine("Hola " + nombre);
+        }
+
+        static int Sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+
+        //Methods in callback way
+        static void Saludar2() => Console.WriteLine("Hola JP");
+
+        static int Suma2(int numero1, int numero2) => numero1 + numero2;
+    }
+}
---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 7;
+            int numero2 = 15;
+
+            int resultado = numero1 + numero2;
+
+            Console.WriteLine(resultado);
+
+            Console.WriteLine(2 - 5);
+
+            Console.WriteLine(numero1 - numero2);
+
+            int numero3 = 12;
+            double numero4 = 8.5;
+
+            Console.WriteLine(numero3 + numero4); //No es buena practica operar distinos tipos de numeros
+
+            Console.WriteLine("Hola" + "Mundo");
+
+            Console.WriteLine('a' + 'a');
+
+            Console.WriteLine("a" + "a");
+
+            Console.WriteLine(8 / 4);
+
+            Console.WriteLine(8.0 / 4.0);
+
+            Console.WriteLine(10.0 / 3.0);
+
+            Console.WriteLine(10.0 / 3); // Mala practica
+
+            Console.WriteLine(2 + 3 * 5); //17
+
+            Console.WriteLine((2 + 3) * 5); // 25
+
+            Console.WriteLine("--------------------------------------");
+
+            //Modulo
+
+            Console.WriteLine(5380 % 30);
+
+            Console.WriteLine(546 / 31); 
+
+            Console.WriteLine("--------------------------------------");
+
+            //Numeros infinitos y NaN Not a Number
+
+            double infinito1 = 5.0 / 0.0;
+
+            Console.WriteLine(infinito1);
+
+            double infinito2 = 0.0 / 0.0;
+
+            Console.WriteLine(infinito2);
+
+            Console.WriteLine("--------------------------------------");
+
+            //Incremento y decremento
+
+            int conteo = 0;
+            Console.WriteLine(conteo); //0
+
+            conteo += 1;
+            Console.WriteLine(conteo); //1
+
+            //Prefijo
+            ++conteo;
+            Console.WriteLine(conteo); //2
+
+            conteo -= 1;
+            Console.WriteLine(conteo); //1
+
+            //Postfijo
+            --conteo;
+            Console.WriteLine(conteo); //0
+
+            int conteo2 = 10;
+
+            Console.WriteLine(conteo2++);
+            Console.WriteLine(conteo2);
+            Console.WriteLine(--conteo2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
